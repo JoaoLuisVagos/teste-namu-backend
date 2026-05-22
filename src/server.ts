6fb02c6fe53c123181318263271e5b618078f7d4
João Luis Vagos <joaoluisvagos@gmail.com>
2026-05-22 09:34:01 -0300

    feat: add linting and formatting scripts, update dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
+import sequelize from './database';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 
 const port = process.env.PORT ?? 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+
+async function bootstrap() {
+	await sequelize.authenticate();
+	app.listen(port, () => console.log(`Server running on port ${port}`));
+}
+
+void bootstrap();
 
 export default app;
